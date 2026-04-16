b6e9db21afdf434d820fdc0b3f120fb11625718f
networkpenetrationtester <77405346+networkpenetrationtester@users.noreply.github.com>
2026-04-16 03:50:17 -0500

    remove shit

--- test-sparkline.ts
+++
@@ -1,6 +0,0 @@
-import { RetryFailedRequest } from "./index.modules.ts";
-import fs from 'node:fs'
-
-let x = await RetryFailedRequest('https://tankionline.com/tutorial/src/resources/movies/finish.swf');
-x && fs.writeFileSync('./test.swf', x);
-!x && console.log('no luck i guess');
